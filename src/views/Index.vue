a8ec148bc6452f64237c34c777a6d626a6b12922
Ruslan Zakharov <Rzakharov90@mail.ru>
2018-05-10 22:24:40 +0300

    edit links on index page

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,29 +1,29 @@
 <template>
     <section>
         <div class="gap">
-            <a class="img-block sm-img-block" href="derevyannyj-brus.html">
+            <router-link  class="img-block sm-img-block" to="/derevyannyi-brus">
                 <img src="/img/main-img1.jpg" class="img-block__img sm-img-block__img" />
                 <div  class="img-block__desc sm-img-block__desc">
                     Деревянный брус
                 </div>
-            </a>
+            </router-link >
         </div>
         <div class="flex-cols sm-flex-cols_wrap">
             <div class="flex-cols__col col-6 sm-col-12 sm-gap">
-                <a class="img-block sm-img-block" href="obreznaya-doska.html">
+                <router-link  class="img-block sm-img-block" to="/obreznaya-doska">
                     <img src="/img/main-img2.jpg" class="img-block__img sm-img-block__img" />
                     <div  class="img-block__desc img-block__desc_small sm-img-block__desc">
                         Обрезная доска
                     </div>
-                </a>
+                </router-link >
             </div>
             <div class="flex-cols__col col-6 sm-col-12">
-                <a class="img-block sm-img-block" href="bruski.html">
+                <router-link  class="img-block sm-img-block" to="/bruski">
                     <img src="/img/main-img3.jpg" class="img-block__img sm-img-block__img" />
                     <div  class="img-block__desc img-block__desc_small sm-img-block__desc">
                         Бруски
                     </div>
-                </a>
+                </router-link >
             </div>
         </div>
     </section>
